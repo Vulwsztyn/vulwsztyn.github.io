09bf7d3c4230d8940571284beb645a5aa1210dd8
Vulwsztyn <artur.mostowski@op.pl>
2018-03-24 09:52:17 +0100

    Adams final chyba final

--- Dwumian/script.js
+++ Dwumian/script.js
@@ -1,82 +1,95 @@
 class Simulation {
     constructor(CA0, k, tau,T, iloscKrokow){
-    this.CA = [[CA0],[0]];
+    this.CA0=CA0;
+    this.CA0s=[];
+    this.CA = [[0],[0]];
     this.CB=[[0],[0]];
+    this.dCA = [[0],[0]];
+    this.dCB=[[0],[0]];
     this.k = k;
     this.tau = tau;
     this.T=T;
 
     this.iloscKrokow=iloscKrokow;
-    this.h=T/iloscKrokow;
+    this.h=this.T/iloscKrokow;
     this.wygenerowane=1;
-}
 
-deltaC(C1,C2,minus,zero){
-    return (C1-zero*C2)/this.tau-minus*this.k*C2;
-}
-
- Runge(C1,C2,minus,zero,ktory){
-    if (typeof zero === 'undefined') { zero = 1; }
-    if (typeof minus === 'undefined') { minus = 1; }
-    if (typeof ktory === 'undefined') { ktory = 2; }
-
-     let k1 = this.deltaC(C1, C2, minus, zero);
-     let k2 = this.deltaC(C1 + this.h / 2, C2 + this.h * k1 / 2, minus, zero);
-     let k3 = this.deltaC(C1 + this.h / 2, C2 + this.h * k2 / 2, minus, zero);
-     let k4 = this.deltaC(C1 + this.h, C2 + this.h * k3, minus, zero);
-     if (ktory===2){
-        return C2+this.h/6*(k1+2*k2+2*k3+k4);
-    }
-    else{
-        return C1+this.h/6*(k1+2*k2+2*k3+k4);
+    for(let i=0;i<iloscKrokow;i++){
+        this.CA0s[i]=CA0;
     }
 }
 
-RungeKutta(i){
-    this.CA[0][i]=(this.Runge(this.CA[0][0],this.CA[0][i-1]));
-    this.CB[0][i]=(this.Runge(this.CB[0][i-1],this.CA[0][i-1],-1,0,1));
-    this.CA[1][i]=(this.Runge(this.CA[0][i-1],this.CA[1][i-1]));
-    this.CB[1][i]=(this.Runge(this.CB[0][i-1],this.CB[1][i-1],-1));
+deltaC(C1,C2,plus,zero){
+    return (C1-zero*C2)/this.tau+plus*this.k*C2;
 }
-Adams(C1,A1,i,minus,zero,ktory){
-    if (typeof zero === 'undefined') { zero = 1; }
-    if (typeof minus === 'undefined') { minus = 1; }
-    if (typeof ktory === 'undefined') { ktory = 2; }
+
+ Runge(C1,C2,plus,zero){
+     let k1 = this.deltaC(C1, C2, plus, zero);
+     let k2 = this.deltaC(C1 + this.h / 2, C2 + k1 / 2, plus, zero);
+     let k3 = this.deltaC(C1 + this.h / 2, C2 +  k2 / 2, plus, zero);
+     let k4 = this.deltaC(C1 + this.h, C2 + k3, plus, zero);
+
+     return this.h/6*(k1+2*k2+2*k3+k4);
+}
+
+Adams(C1,A1,i,plus,zero){
     let ky=[];
     for (let j=0;j<4;j++){
-        ky[j]=this.deltaC(C1[i-1-j],A1[i-j-1],minus,zero);
+        ky[j]=this.deltaC(C1[i-1-j],A1[i-j-1],plus,zero);
     }
     let wspolczynniki=[55/24,-59/24,37/24,-9/24];
     let coDodac=0;
     for (let j=0;j<4;j++){
         coDodac+=wspolczynniki[j]*ky[j];
     }
-    coDodac*=this.h;
-    if (ktory===2){
-        return A1[i-1]+coDodac;
-    }
-    else{
-        return C1[i-1]+coDodac;
-    }
+    return coDodac*this.h;
 }
+
+RungeKutta(i){
+    this.dCA[0][i]=(this.Runge(this.CA0,this.CA[0][i-1],-1,1));
+    this.dCB[0][i]=(this.Runge(this.CB[0][i-1],this.CA[0][i-1],1,0));
+    this.dCA[1][i]=(this.Runge(this.CA[0][i-1],this.CA[1][i-1],-1,1));
+    this.dCB[1][i]=(this.Runge(this.CB[0][i-1],this.CB[1][i-1],1,1));
+
+    this.CA[0][i]=this.CA[0][i-1]+this.dCA[0][i];
+    this.CB[0][i]=this.CB[0][i-1]+this.dCB[0][i];
+    this.CA[1][i]=this.CA[1][i-1]+this.dCA[1][i];
+    this.CB[1][i]=this.CB[1][i-1]+this.dCB[1][i];
+}
+
 AdamsBashfort(i){
-    this.CA[0][i]=(this.Adams(this.CA[0],this.CA[0],i));
-    this.CB[0][i]=(this.Adams(this.CB[0],this.CA[0],i,-1,0,1));
-    this.CA[1][i]=(this.Adams(this.CA[0],this.CA[1],i));
-    this.CB[1][i]=(this.Adams(this.CB[0],this.CB[1],i,-1));
+    this.dCA[0][i]=(this.Adams(this.CA0s,this.CA[0],i,-1,1));
+    this.dCB[0][i]=(this.Adams(this.CB[0],this.CA[0],i,1,0));
+    this.dCA[1][i]=(this.Adams(this.CA[0],this.CA[1],i,-1,1));
+    this.dCB[1][i]=(this.Adams(this.CB[0],this.CB[1],i,1,1));
+
+    this.CA[0][i]=this.CA[0][i-1]+this.dCA[0][i];
+    this.CB[0][i]=this.CB[0][i-1]+this.dCB[0][i];
+    this.CA[1][i]=this.CA[1][i-1]+this.dCA[1][i];
+    this.CB[1][i]=this.CB[1][i-1]+this.dCB[1][i];
 }
 
 runguj(j){
     for(let i=this.wygenerowane; i<j; i++){
         this.RungeKutta(i);
     }
-    this.wygenerowane=j;
+    if(j>this.wygenerowane) this.wygenerowane=j;
+
+    this.dCA[0][0]=this.dCA[0][1];
+    this.dCB[0][0]=this.dCB[0][1];
+    this.dCA[1][0]=this.dCA[1][1];
+    this.dCB[1][0]=this.dCB[1][1];
 }
 adamsuj(j){
     for(let i=this.wygenerowane; i<j; i++){
         this.AdamsBashfort(i);
     }
-    this.wygenerowane=j;
+    if(j>this.wygenerowane) this.wygenerowane=j;
+
+    this.dCA[0][0]=this.dCA[0][1];
+    this.dCB[0][0]=this.dCB[0][1];
+    this.dCA[1][0]=this.dCA[1][1];
+    this.dCB[1][0]=this.dCB[1][1];
 }
 
     wypis(){
@@ -88,18 +101,12 @@ adamsuj(j){
 }
 
 function init() {
-    var ileChartow=8;
-
-    let symulacja= new Simulation(5,0.12,5,4,100);
-    symulacja.runguj(4);
-    symulacja.adamsuj(symulacja.iloscKrokow);
-    symulacja.wypis();
+    var ileChartow=10;
 
     var data=[];
     var options=[];
     var chart=[];
     for(let i=0;i<ileChartow;i++){
-         data[i] = new google.visualization.DataTable();
          options[i] = {
             chart: {
                 title: 'Adams-Bashfort łącznie'
@@ -120,32 +127,33 @@ function init() {
              }
         };
          let nazwa='Wykres'+i;
-
         chart[i] = new google.charts.Line(document.getElementById(nazwa));
     }
 
-    options[1].chart.title='Adams-Bashfort osobno 1';
-    options[2].chart.title='Adams-Bashfort osobno 2';
-    options[3].chart.title='Adams-Bashfort osobno 2 skala ta co w reszcie';
-    options[4].chart.title='Runge-Kutta łącznie';
-    options[5].chart.title='Runge-Kutta osobno 1';
-    options[6].chart.title='Runge-Kutta osobno 2';
+    options[2].chart.title='Adams-Bashfort osobno 1';
+    options[4].chart.title='Adams-Bashfort osobno 2';
+    options[6].chart.title='Adams-Bashfort osobno 2 skala ta co w reszcie';
+    options[1].chart.title='Runge-Kutta łącznie';
+    options[3].chart.title='Runge-Kutta osobno 1';
+    options[5].chart.title='Runge-Kutta osobno 2';
     options[7].chart.title='Runge-Kutta osobno 2 skala ta co w reszcie';
-    options[3].vAxis.viewWindow={};
+    options[8].chart.title='Runge-Kutta Delty';
+    options[9].chart.title='Adams-Bashfort Delty';
+    options[6].vAxis.viewWindow={};
     options[7].vAxis.viewWindow={};
-    options[3].vAxis.viewWindow.max=parseInt(document.getElementById('CA0').value);
+    options[6].vAxis.viewWindow.max=parseInt(document.getElementById('CA0').value);
     options[7].vAxis.viewWindow.max=parseInt(document.getElementById('CA0').value);
 
     var button = document.getElementById('but');
 
     function drawChart() {
-
        for(let i=0;i<ileChartow;i++) {
-            chart[i].draw(data[i], google.charts.Line.convertOptions(options[i]));
-        }
+           chart[i].draw(data[i], google.charts.Line.convertOptions(options[i]));
+       }
+
     }
     function Klik(){
-        console.log("adolf");
+        ileChartow=ileChartow+2*2-4;
         let ca0 = parseFloat(document.getElementById('CA0').value);
         let k = parseFloat(document.getElementById('k').value);
         let T = parseFloat(document.getElementById('T').value);
@@ -162,46 +170,58 @@ function init() {
             data[i].addColumn('number', opisOsiX);
         }
         for(let i=0;i<2;i++){
-            data[ileChartow/2*i].addColumn('number', 'CA1');
-            data[ileChartow/2*i].addColumn('number', 'CB1');
-            data[ileChartow/2*i].addColumn('number', 'CA2');
-            data[ileChartow/2*i].addColumn('number', 'CB2');
-
-            data[ileChartow/2*i+1].addColumn('number', 'CA1');
-            data[ileChartow/2*i+1].addColumn('number', 'CB1');
-
-            data[ileChartow/2*i+2].addColumn('number', 'CA2');
-            data[ileChartow/2*i+2].addColumn('number', 'CB2');
-
-            data[ileChartow/2*i+3].addColumn('number', 'CA2');
-            data[ileChartow/2*i+3].addColumn('number', 'CB2');
-
+            let j=i;
+            data[j].addColumn('number', 'CA1');
+            data[j].addColumn('number', 'CB1');
+            data[j].addColumn('number', 'CA2');
+            data[j].addColumn('number', 'CB2');
+            j=i+2;
+            data[j].addColumn('number', 'CA1');
+            data[j].addColumn('number', 'CB1');
+            j=i+4;
+            data[j].addColumn('number', 'CA2');
+            data[j].addColumn('number', 'CB2');
+            j=i+6;
+            data[j].addColumn('number', 'CA2');
+            data[j].addColumn('number', 'CB2');
+            j=i+8;
+            data[j].addColumn('number', 'dCA1');
+            data[j].addColumn('number', 'dCB1');
+            data[j].addColumn('number', 'dCA2');
+            data[j].addColumn('number', 'dCB2');
         }
+
 
         for(let i=0; i<sym.iloscKrokow; i++) {
             data[0].addRows([
                 [i * sym.h, sym.CA[0][i], sym.CB[0][i], sym.CA[1][i], sym.CB[1][i]]
             ]);
             data[1].addRows([
-                [i * sym.h, sym.CA[0][i], sym.CB[0][i]]
-            ]);
-            data[2].addRows([
-                [i * sym.h, sym.CA[1][i], sym.CB[1][i]]
-            ]);
-            data[3].addRows([
-                [i * sym.h, sym.CA[1][i], sym.CB[1][i]]
-            ]);
-            data[4].addRows([
                 [i * sym2.h, sym2.CA[0][i], sym2.CB[0][i], sym2.CA[1][i], sym2.CB[1][i]]
             ]);
-            data[5].addRows([
+            data[2].addRows([
                 [i * sym.h, sym.CA[0][i], sym.CB[0][i]]
+            ]);
+            data[3].addRows([
+                [i * sym2.h, sym2.CA[0][i], sym2.CB[0][i]]
+            ]);
+            data[4].addRows([
+                [i * sym.h, sym.CA[1][i], sym.CB[1][i]]
+            ]);
+            data[5].addRows([
+                [i * sym2.h, sym2.CA[1][i], sym2.CB[1][i]]
             ]);
             data[6].addRows([
                 [i * sym.h,sym.CA[1][i], sym.CB[1][i]]
             ]);
             data[7].addRows([
-                [i * sym.h,sym.CA[1][i], sym.CB[1][i]]
+                [i * sym2.h,sym2.CA[1][i], sym2.CB[1][i]]
+            ]);
+            data[8].addRows([
+                [i * sym.h, sym2.dCA[0][i], sym2.dCB[0][i], sym2.dCA[1][i], sym2.dCB[1][i]]
+            ]);
+            data[9].addRows([
+                [i * sym.h, sym.dCA[0][i], sym.dCB[0][i], sym.dCA[1][i], sym.dCB[1][i]]
             ]);
         }
         drawChart();
