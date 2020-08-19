2bf9e7c86090aa6e0c7cd1b6328c361acd0b5996
Artur Mostowski <artur.mostowski@currency-one.com>
2020-08-20 00:43:03 +0200

    gory 2020

---
+++ gory/precache-manifest.ee725a3f0d1521b68269925b33610f76.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "196c29480cd44572412a224c2cadec25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55b98a315fb257e76fc5",
+    "url": "/static/css/main.07f1fd89.chunk.css"
+  },
+  {
+    "revision": "647eb34b04da82695daa",
+    "url": "/static/js/2.a8b4905f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a8b4905f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55b98a315fb257e76fc5",
+    "url": "/static/js/main.f661be86.chunk.js"
+  },
+  {
+    "revision": "050fbff4529df62c108a",
+    "url": "/static/js/runtime-main.8af42973.js"
+  },
+  {
+    "revision": "dd1a391d929ce507bb8d50e870a41b06",
+    "url": "/static/media/dojazd11.dd1a391d.png"
+  },
+  {
+    "revision": "7667220ba66dfe322cb355ba19446a4a",
+    "url": "/static/media/dojazd12.7667220b.png"
+  },
+  {
+    "revision": "9af27f228f01d1a5ef923ac48b537ce5",
+    "url": "/static/media/logo.9af27f22.png"
+  }
+]);
